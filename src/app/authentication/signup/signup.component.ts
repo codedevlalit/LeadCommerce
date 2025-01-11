131a5b6c90226da97e4aef5de9f1aed29769cff0
Lalit Saini <codedev@lalitsaini.com>
2025-01-11 08:18:03 +0530

    review signup and add comments

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
+// Decorator that marks a class as an Angular component and provides configuration metadata
 @Component({
+  // The CSS selector that identifies this component in a template
   selector: 'app-signup',
+  // List of modules or components to import
   imports: [],
+  // Path to the HTML template file for this component
   templateUrl: './signup.component.html',
+  // Path to the CSS file for this component
   styleUrl: './signup.component.css'
 })
+// The class that defines the component's behavior
 export class SignupComponent {
 
 }
