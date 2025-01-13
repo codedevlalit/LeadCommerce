f2f35ac48b6c3e59225623badba70383963e221f
dev@tusharupadhyay.com <dev@tusharupadhyay.com>
2025-01-13 10:03:37 +0530

    login - email and phone difference logic implementation

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonFunctions } from '../../core/utilities/common.functions';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule], // Import CommonModule and FormsModule
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  emailOrPhoneNumberText: any = '';
+  otp: string = '';
+  isOtpvisible: boolean = false;
+
+
+
+  onContinueButtonClicked() 
+  {
+    debugger; 
+    if(CommonFunctions.isNullOrUndefinedOrEmpty(this.emailOrPhoneNumberText))
+      {
+        alert('Email or Phone Number is required');
+        return;
+      }
+
+      if(this.emailOrPhoneNumberText.includes('@'))
+      {
+        // Send OTP to email
+      }else
+      {
+        // Send OTP to phone number
+      }
+  }
+
+
 }
+
+
